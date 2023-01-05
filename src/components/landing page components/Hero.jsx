8e1bb5e11525c1b410abcd0829d58fdc3e9d18e3
abrusu-collins <snillochemingson@gmail.com>
2023-01-05 13:56:11 +0000

    cool

--- src/components/landing page components/Hero.jsx
+++ src/components/landing page components/Hero.jsx
@@ -1,7 +1,16 @@
+import heroImg from "../../imgs/hero-img.png";
+
 function Hero() {
-    return ( 
-        <div className="hero"></div>
-     );
+  return (
+    <div className="hero">
+      <div className="hero-text">
+        <p className="hero-title">Bibly</p>
+        <p className="hero-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quod facere, officiis explicabo ut illum laudantium recusandae dignissimos reprehenderit! Molestiae facere dolores nemo natus delectus veritatis atque temporibus maiores ea.</p>
+      </div>
+
+      <img src={heroImg} alt="heroImage" />
+    </div>
+  );
 }
 
 export default Hero;
