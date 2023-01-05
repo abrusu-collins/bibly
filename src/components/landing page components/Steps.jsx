80e42b657c6c4f1e01f49572cde7350d856d350f
abrusu-collins <snillochemingson@gmail.com>
2023-01-05 19:33:30 +0000

    beauty

--- src/components/landing page components/Steps.jsx
+++ src/components/landing page components/Steps.jsx
@@ -1,8 +1,51 @@
 function Steps() {
-    return ( 
+  return (
     <div className="steps">
-        
-    </div> );
+      <p className="header">Steps to generate your Image</p>
+
+      <div className="steps-div">
+        <div className="step1">
+          <p className="number">1</p>
+          <p className="step-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+            sapiente nulla adipisci non repudiandae, animi aliquam quae
+            cupiditate, cumque iusto ratione corporis delectus aut eos pariatur?
+            Aperiam magnam voluptas dolor.
+          </p>
+        </div>
+
+        <div className="step1">
+          <p className="number">2</p>
+          <p className="step-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+            sapiente nulla adipisci non repudiandae, animi aliquam quae
+            cupiditate, cumque iusto ratione corporis delectus aut eos pariatur?
+            Aperiam magnam voluptas dolor.
+          </p>
+        </div>
+
+        <div className="step1">
+          <p className="number">3</p>
+          <p className="step-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+            sapiente nulla adipisci non repudiandae, animi aliquam quae
+            cupiditate, cumque iusto ratione corporis delectus aut eos pariatur?
+            Aperiam magnam voluptas dolor.
+          </p>
+        </div>
+
+        <div className="step1">
+          <p className="number">4</p>
+          <p className="step-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
+            sapiente nulla adipisci non repudiandae, animi aliquam quae
+            cupiditate, cumque iusto ratione corporis delectus aut eos pariatur?
+            Aperiam magnam voluptas dolor.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Steps;
