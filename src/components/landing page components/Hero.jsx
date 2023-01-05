63918b0ba35a16371ec485df87bf5da6ddf6e254
abrusu-collins <snillochemingson@gmail.com>
2023-01-05 14:15:48 +0000

    cool

--- src/components/landing page components/Hero.jsx
+++ src/components/landing page components/Hero.jsx
@@ -6,6 +6,7 @@ function Hero() {
       <div className="hero-text">
         <p className="hero-title">Bibly</p>
         <p className="hero-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quod facere, officiis explicabo ut illum laudantium recusandae dignissimos reprehenderit! Molestiae facere dolores nemo natus delectus veritatis atque temporibus maiores ea.</p>
+      <a href="">Start Generator</a>
       </div>
 
       <img src={heroImg} alt="heroImage" />
