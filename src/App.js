603c1d400b97006ce69fe53e81332ed1c49f7611
abrusu-collins <snillochemingson@gmail.com>
2023-01-05 19:07:41 +0000

    beautiful

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles/index.css";
 import Hero from "./components/landing page components/Hero";
 import Marquee from "./components/landing page components/Marquee";
+import Steps from "./components/landing page components/Steps";
 function App() {
   return (
    <>
    <Hero/>
    <Marquee/>
+   <Steps/>
    </>
   );
 }
