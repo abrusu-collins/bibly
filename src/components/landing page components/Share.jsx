a0eeb545fba358adadeccd741a3fb1a5de6edffd
abrusu-collins <snillochemingson@gmail.com>
2023-01-07 09:15:21 +0000

    neat

--- src/components/landing page components/Share.jsx
+++ src/components/landing page components/Share.jsx
@@ -1,7 +1,20 @@
+import facebook from "../../imgs/facebook.png";
+import instagram from "../../imgs/instagram.png";
+import twitter from "../../imgs/twitter.png";
+import snapchat from "../../imgs/snapchat.png";
+
 function Share() {
-    return ( <div className="share">
-        
-    </div> );
+  return (
+    <div className="share">
+      <p className="header">Instantly share to social media</p>
+      <div className="socials">
+        <img src={facebook} alt="facebook" />
+        <img src={instagram} alt="instagram" />
+        <img src={twitter} alt="twitter" />
+        <img src={snapchat} alt="snapchat" />
+      </div>
+    </div>
+  );
 }
 
 export default Share;
