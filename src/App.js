bd4b2475411747d0a735bf9362b7fc75e0aa9ddf
abrusu-collins <snillochemingson@gmail.com>
2023-01-08 14:18:46 +0000

    router works

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import "./styles/index.css";
-import Hero from "./components/landing page components/Hero";
-import Marquee from "./components/landing page components/Marquee";
-import Steps from "./components/landing page components/Steps";
-import Share from "./components/landing page components/Share";
-import Footer from "./components/landing page components/Footer";
+import Home from "./components/landing page components/Home";
 function App() {
   return (
-   <>
-   <Hero/>
-   <Marquee/>
-   <Steps/>
-   <Share/>
-   <Footer/>
-   </>
+    <>
+      <Home />
+    </>
   );
 }
 
---
+++ src/components/landing page components/Home.jsx
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Marquee from "./Marquee";
+import Steps from "./Steps";
+import Share from "./Share";
+import Footer from "./Footer";
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Marquee />
+      <Steps />
+      <Share />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
+<></>;
