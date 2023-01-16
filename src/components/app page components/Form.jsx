f7921e09e851fafa28e5ebef0d15960ba3f13e2a
abrusu-collins <snillochemingson@gmail.com>
2023-01-16 16:18:58 +0000

    form work

--- src/components/app page components/Form.jsx
+++ src/components/app page components/Form.jsx
@@ -4,7 +4,7 @@ function Form() {
         <input type="text" name="book" id="book" placeholder="book name e.g mark, luke..." />
         <input type="text" name="chapter" id="chapter" placeholder="chapter number e.g 1, 2..." />
         <input type="text" name="verse" id="verse" placeholder="verse number e.g 1, 2..." />
-        <button type="submit">Generate</button>
+        <button type="submit">Generate verse</button>
     </form>
     </> );
 }
