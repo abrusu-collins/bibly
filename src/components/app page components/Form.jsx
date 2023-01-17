15ae9074c0704affbe75bbb951b9fde0961a558a
abrusu-collins <snillochemingson@gmail.com>
2023-01-17 00:11:53 +0000

    neat edits

--- src/components/app page components/Form.jsx
+++ src/components/app page components/Form.jsx
@@ -14,9 +14,9 @@ function Form() {
     }
     return ( <>
     <form >
-        <input type="text" name="book" id="book" placeholder="book name e.g mark, luke..." value={userinputs.book} onChange={(e)=>{setuserinputs({...userinputs,book:e.target.value})}}  />
-        <input type="text" name="chapter" id="chapter" placeholder="chapter number e.g 1, 2..." value={userinputs.chapter} onChange={(e)=>{setuserinputs({...userinputs,chapter:e.target.value})}} />
-        <input type="text" name="verse" id="verse" placeholder="verse number e.g 1, 2..." value={userinputs.verse} onChange={(e)=>{setuserinputs({...userinputs,verse:e.target.value})}} />
+        <input type="text" name="book" id="book" placeholder="book name e.g mark, 1 thessalonians, ..." value={userinputs.book} onChange={(e)=>{setuserinputs({...userinputs,book:e.target.value})}}  />
+        <input type="text" name="chapter" id="chapter" placeholder="chapter number e.g 1, 2, ..." value={userinputs.chapter} onChange={(e)=>{setuserinputs({...userinputs,chapter:e.target.value})}} />
+        <input type="text" name="verse" id="verse" placeholder="verse number e.g 1, 2, ..." value={userinputs.verse} onChange={(e)=>{setuserinputs({...userinputs,verse:e.target.value})}} />
         <button type="submit" onClick={generate}>Generate verse</button>
         <br />
 
