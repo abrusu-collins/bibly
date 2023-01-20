694fd599b29be5dda23d66dc6d1c7fc4cb4389ef
abrusu-collins <snillochemingson@gmail.com>
2023-01-20 13:57:16 +0000

    making sense

--- src/components/app page components/DataAndEditing.jsx
+++ src/components/app page components/DataAndEditing.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-
+import a from "../../json files/backgrounds.json";
 function DataAndEditing() {
     const verse = useSelector((state) => state.verse.verse);
     const reference = useSelector((state) => state.verse.reference);
@@ -16,7 +16,9 @@ function DataAndEditing() {
 
       <div className="editing">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quod ratione exercitationem dolor corrupti corporis sint ex consequuntur officiis sed! Exercitationem obcaecati incidunt suscipit nobis optio culpa officiis itaque porro?
-          
+          {a.map((aa)=>{
+            return <div key={aa.id} style={{"backgroundImage":aa.css}}>{aa.css}</div>
+          })}
         </p>
       </div>
     </div>
