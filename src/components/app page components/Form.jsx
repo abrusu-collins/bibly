6d4232fe8bc5f8a4b1cfc70033d4414ea9273771
abrusu-collins <snillochemingson@gmail.com>
2023-02-11 00:23:16 +0000

    done for today

--- src/components/app page components/Form.jsx
+++ src/components/app page components/Form.jsx
@@ -9,14 +9,37 @@ function Form() {
     const dispach = useDispatch()
     const generate = (e)=>{
         e.preventDefault();
-        fetch(`https://bible-api.com/${userinputs.book}%20${userinputs.chapter}:${userinputs.verse}`).then((response)=>{return response.json()}).then((data)=>{console.log(data.text);dispach(changeVerse(data));dispach(changeReference(data))})
+        fetch(`https://bible-api.com/${userinputs.book}%20${userinputs.chapter}:${userinputs.verse}`)
+        .then((response)=>{return response.json()})
+        .then((data)=>{console.log(data.text);dispach(changeVerse(data));dispach(changeReference(data))})
 
     }
     return ( <>
     <form >
-        <input type="text" name="book" id="book" placeholder="book name e.g mark, 1 thessalonians, ..." value={userinputs.book} onChange={(e)=>{setuserinputs({...userinputs,book:e.target.value})}}  />
-        <input type="text" name="chapter" id="chapter" placeholder="chapter number e.g 1, 2, ..." value={userinputs.chapter} onChange={(e)=>{setuserinputs({...userinputs,chapter:e.target.value})}} />
-        <input type="text" name="verse" id="verse" placeholder="verse number e.g 1, 2, ..." value={userinputs.verse} onChange={(e)=>{setuserinputs({...userinputs,verse:e.target.value})}} />
+        <input 
+        type="text"
+        name="book" 
+        id="book" 
+        placeholder="book name e.g mark, 1 thessalonians, ..." 
+        value={userinputs.book} 
+        onChange={(e)=>{setuserinputs({...userinputs,book:e.target.value})}}  />
+
+        <input 
+        type="text" 
+        name="chapter" 
+        id="chapter" 
+        placeholder="chapter number e.g 1, 2, ..." 
+        value={userinputs.chapter} 
+        onChange={(e)=>{setuserinputs({...userinputs,chapter:e.target.value})}} />
+
+        <input 
+        type="text" 
+        name="verse" 
+        id="verse" 
+        placeholder="verse number e.g 1, 2, ..." 
+        value={userinputs.verse} 
+        onChange={(e)=>{setuserinputs({...userinputs,verse:e.target.value})}} />
+        
         <button type="submit" onClick={generate}>Generate verse</button>
         <br />
 
