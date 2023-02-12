39d37bf3d51de271511777d034a1ea721801e222
abrusu-collins <snillochemingson@gmail.com>
2023-02-12 21:28:24 +0000

    working fine

--- src/components/app page components/Form.jsx
+++ src/components/app page components/Form.jsx
@@ -13,6 +13,7 @@ function Form() {
     const [isSelfVerse,setIsSelfVerse] = useState(true);
     const [isTopicVerse,setIsTopicVerse] = useState(false);
     const [isDiscoverVerse,setIsDiscoverVerse] = useState(false);
+    const [topic, setTopic]= useState("");
 
 
 
@@ -40,12 +41,23 @@ function Form() {
             .then((data)=>{console.log(data.text);dispach(changeVerse(data));dispach(changeReference(data))})
         }
         else if(isTopicVerse){
+            //end with -
+            //end with string
+            //end with :
             openai.createCompletion({
                 model: "text-davinci-003",
-                prompt: "Say this is a test",
-                temperature: 0,
+                prompt: `give me a bible verse reference that teaches on the topic ${topic}. The refrence should be in this form ("mattew 2:22" or "mattew 5:27-28"), please don't add anything to it, just give me the reference`,
+                temperature: 1,
                 max_tokens: 7,
-              }).then((res)=>{return res}).then((data)=>{console.log(data);});
+              })
+              .then((res)=>{return res})
+              .then((data)=>{
+                fetch(`https://bible-api.com/${data.data.choices[0].text}`)
+                .then((response)=>{return response.json()})
+                .then((data)=>{console.log(data.text);dispach(changeVerse(data));dispach(changeReference(data))})
+                
+                console.log(data.data.choices[0].text);});
+
         }
         else{
             
@@ -60,7 +72,7 @@ function Form() {
             <button onClick={activateDiscoverVerse}>Discover random verse</button>
         </div>
       </div>
-      
+
     {isSelfVerse && <form  className="self-verse">
         <input 
         type="text"
@@ -89,13 +101,15 @@ function Form() {
         <button type="submit" onClick={generate}>Generate verse</button>
         <br />
 
-    </form>}
+    </form>
+    }
 
 
     {isTopicVerse && <form className="topic-verse">
-        <input type="text" name="" id="" />
+        <input type="text" name="" id="" onChange={(e)=>{setTopic(e.currentTarget.value)}} />
         <button type="submit" onClick={generate}> Generate verse</button>
-        </form>}
+        </form>
+    }
 
 
     {isDiscoverVerse && <form className="discover-verse">
@@ -103,7 +117,8 @@ function Form() {
             Just click on the "Discover verse" button below to generate a random verse
         </p>
         <button type="submit" onClick={generate}>Discover Verse</button>
-        </form>}
+        </form>
+    }
 
     </> );
 }
