0f57fa52862f18438bcbb07a6d7d485979eb7c1f
abrusu-collins <snillochemingson@gmail.com>
2023-02-18 11:58:30 +0000

    UI sweeter that sugar

--- src/components/landing page components/Steps.jsx
+++ src/components/landing page components/Steps.jsx
@@ -9,8 +9,8 @@ function Steps() {
       </div>
       <div className="steps-div">
         <div className="step1">
+        <p className="number">1</p>
             <p className="step-header">Click on the "Start Generator" button</p>
-          <p className="number">1</p>
           <p className="step-description">
           Are you ready to create something amazing? 
           Click the "start generator" button at the top to kick off the process
@@ -19,8 +19,8 @@ function Steps() {
         </div>
 
         <div className="step1">
+        <p className="number">2</p>
         <p className="step-header">Choose your bible verse</p>
-          <p className="number">2</p>
           <p className="step-description">
             You'll be taken to the app page. On the app page, you can 
             generate your verse by using a reference, topic or you 
@@ -29,8 +29,8 @@ function Steps() {
         </div>
 
         <div className="step1">
+        <p className="number">3</p>
         <p className="step-header">Customize your image</p>
-          <p className="number">3</p>
           <p className="step-description">
              After generating your verse, you have options to edit the appearance 
              of the verse to make it more pleasing and enticing.
@@ -39,8 +39,8 @@ function Steps() {
         </div>
 
         <div className="step1">
+        <p className="number">4</p>
         <p className="step-header">Save or Share</p>
-          <p className="number">4</p>
           <p className="step-description">
         After generating and editing, you can now download the image and share on your 
         preferred social media site or do whatever you want to do with it.
