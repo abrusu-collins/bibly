575362b5e19bd7485c0ea1dee01228a396bc4845
abrusu-collins <snillochemingson@gmail.com>
2023-02-18 10:01:06 +0000

    uninstalled ga

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/index.css";
 import Home from "./components/landing page components/Home";
 import MainApp from "./components/app page components/MainApp";
-import ReactGA from 'react-ga';
 
-const MEASUREMENT_ID = "G-60BQ80SNDW"; 
-ReactGA.initialize(MEASUREMENT_ID);
 function App() {
   return (
     <>
--- src/components/app page components/MainApp.jsx
+++ src/components/app page components/MainApp.jsx
@@ -1,13 +1,10 @@
 import Form from "./Form";
 import DataAndEditing from "./DataAndEditing";
 import { useEffect } from "react";
-import ReactGA from 'react-ga';
+
 
 
 function MainApp() {
-  useEffect(()=>{
-    ReactGA.pageview(window.location.pathname);
-  },[])
   return (
     <>
       <Form />
